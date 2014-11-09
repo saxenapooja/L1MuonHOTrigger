2d29b9149ba5a7bd575ab3cea9cfd125300c201b
Pooja Saxena <pooja.saxena@cern.ch>
2014-11-09 12:38:07 +0100

    added the HO variables

--- python/MBLTProducer_cfi.py
+++ python/MBLTProducer_cfi.py
@@ -3,5 +3,7 @@ import FWCore.ParameterSet.Config as cms
 MBLTProducer = cms.EDProducer(
     'MBLTProducer',
     TriggerPrimitiveSrc = cms.InputTag('L1ITMuTriggerPrimitives'),
-    MaxDeltaPhi = cms.double( 0.05 )
+    MaxDeltaPhi = cms.double( 0.05 ),
+    AddHO  = cms.bool(false),
+    MaxStationForHO = cms.int(3)
     )
