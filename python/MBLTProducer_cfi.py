c814afd3c831e6f2c6d0fb0656c4c4e1df45ed98
Pooja Saxena <pooja.saxena@cern.ch>
2014-11-12 15:10:24 +0100

    added deltaPhiWithHO variables

--- python/MBLTProducer_cfi.py
+++ python/MBLTProducer_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 MBLTProducer = cms.EDProducer(
     'MBLTProducer',
     TriggerPrimitiveSrc = cms.InputTag('L1ITMuTriggerPrimitives'),
-    MaxDeltaPhi = cms.double( 0.05 ),
-    AddHO  = cms.bool(false),
-    MaxStationForHO = cms.int(3)
+    MaxDeltaPhi         = cms.double( 0.05 ),
+    MaxDeltaPhiForHO    = cms.double( 0.5 ),
+    AddHO               = cms.bool(False),
+    MaxStationForHO     = cms.int32(3)
     )
